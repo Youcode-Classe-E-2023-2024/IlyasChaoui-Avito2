4f0891d67f9d8fe4928a9a4c53e4026b90ed6211
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-11-27 21:17:17 +0100

    Update UserProfile.php
    add session to href userprofile

--- pages/UserProfile.php
+++ pages/UserProfile.php
@@ -100,7 +100,7 @@ $result = mysqli_query($connection, $query);
     <aside class="h-screen z-20 fixed w-16 flex flex-col space-y-10 items-center justify-center  bg-gray-800 text-white">
 
         <!-- Courses -->
-        <a href="./Annoncer.php">
+        <a href="./<?php echo $_SESSION['role']; ?>.php">
             <div class="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
