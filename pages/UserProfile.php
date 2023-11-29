92cd05c34f7793a56c2dbde58177f6ae5a5e2a02
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-11-29 21:36:57 +0100

    Update UserProfile.php

--- pages/UserProfile.php
+++ pages/UserProfile.php
@@ -21,6 +21,7 @@ $row = mysqli_fetch_assoc($result);
 
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/Viewer.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -136,8 +137,9 @@ $row = mysqli_fetch_assoc($result);
                         </div>
                     </a>
                     ';
-                    } else {
-                        echo '<div class="h-10 w-8" >  </div>';
+                    } elseif($_SESSION['role'] == 'Viewer') {
+                        echo '<div class="h-10 w-96" >  </div>
+                        <div class="h-10 w-28" >  </div>';
                     }
 
                     ?>
@@ -162,7 +164,14 @@ $row = mysqli_fetch_assoc($result);
                             <?php echo $_SESSION['role']; ?>
                         </div>
                     </div>
-                    <div class="mx-4 h-10 w-10 rounded-full cursor-pointer bg-gray-200 border-2 border-blue-400"></div>
+                    <div
+                        class="relative mx-4 h-10 w-10  cursor-pointer bg-transparent ">
+                        <img src="<?php echo $_SESSION['UserImage']; ?>"
+                            class="sasawi  shadow-2xl hidden lg:block">
+                    </div>
+                    <div class="">
+                        <div class="sansan"></div>
+                    </div>
                 </div>
 
             </div>
