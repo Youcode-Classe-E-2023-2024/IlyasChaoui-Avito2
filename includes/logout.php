8ff93ab5fb1c09d4e6879883e22936c9332a73a3
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-11-29 01:31:49 +0100

    Update logout.php

--- includes/logout.php
+++ includes/logout.php
@@ -3,7 +3,7 @@ session_start();
 unset($_SESSION['user']);
 session_destroy();
 if (empty($_SESSION['user'])) {
-    header('Location: ../Login.php');
+    header('Location: ../index.php');
     echo '<script>alert("You are loged out.")</script>';
 }
 ?>
