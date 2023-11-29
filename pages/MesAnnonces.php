abf61aaf6cfb1336658636218e9494f5b206a008
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-11-29 21:36:54 +0100

    Update MesAnnonces.php

--- pages/MesAnnonces.php
+++ pages/MesAnnonces.php
@@ -102,7 +102,7 @@ $_SESSION['num_annonce'] = mysqli_num_rows($result);
                 </div>
 
                 <!-- Texto -->
-                <div class="flex flex-row items-end " style="margin-left: 335px;margin-right: -50px;">
+                <div class="flex flex-row items-end " style="margin-left: 335px;margin-right: -68px;">
                     <div>
                         <!-- Nome -->
                         <div class="text-md font-medium text-gray-500 ">
@@ -113,7 +113,14 @@ $_SESSION['num_annonce'] = mysqli_num_rows($result);
                             <?php echo $_SESSION['role']; ?>
                         </div>
                     </div>
-                    <div class="mx-4 h-10 w-10 rounded-full cursor-pointer bg-gray-200 border-2 border-blue-400"></div>
+                    <div
+                        class="relative mx-4 h-10 w-10  cursor-pointer bg-transparent ">
+                        <img src="<?php echo $_SESSION['UserImage']; ?>"
+                            class="sasawi  shadow-2xl hidden lg:block">
+                    </div>
+                    <div class="">
+                        <div class="sansan"></div>
+                    </div>
                 </div>
 
             </div>
